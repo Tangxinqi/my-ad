e1c10897d4fd0afbe4aa0998270f788fd3887a63
Tangxinqi <1126746915@qq.com>
2021-11-19 15:49:39 +0800

    初始化display和mixer

---
+++ ad-display/src/main/java/com/txq/ad/display/StartApplication.java
@@ -0,0 +1,12 @@
+package com.txq.ad.display;
+
+/**
+ * @author xinqitang
+ * @date 2021/11/19
+ */
+public class StartApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ ad-display/src/main/java/com/txq/ad/display/controller/SdkController.java
@@ -0,0 +1,8 @@
+package com.txq.ad.display.controller;
+
+/**
+ * @author xinqitang
+ * @date 2021/11/19
+ */
+public class SdkController {
+}
